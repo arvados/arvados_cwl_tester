acf62b07cdd4019577dfdcf8466fe54c8087198a
Monika Krzyżanowska <monigenomi@gmail.com>
2022-10-03 14:25:02 +0200

    Installation

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
-pip install -y pytest cwltool pyyaml arvados-python-client
+set -eox
+
+pip install pytest cwltool pyyaml arvados-python-client
 
 # For fixing bug in libraries
 pip uninstall pycurl
-export PYCURL_SSL_LIBRARY=nss
-pip install --compile --install-option="--with-nss" --no-cache-dir pycurl==7.44
+export PYCURL_SSL_LIBRARY=openssl
+pip install --compile  --no-cache-dir pycurl==7.44
